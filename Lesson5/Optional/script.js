551a8cfd575c300b51ad783da45225e56d140ec4
Yakushenko Evgeniy <yakushenko.evgeny@gmail.com>
2019-04-25 16:43:47 +0300

    add another funcs Lesson5 optional

--- Lesson5/Optional/script.js
+++ Lesson5/Optional/script.js
@@ -1,15 +1,11 @@
 // Выведите на страницу текущую дату и время в формате '09:59:59 30.05.2018'
-let dt = new Date();
-let year = "" + dt.getFullYear();
-year = "0".repeat(4 - year.length) + year;
+let dtNow = new Date();
 
-let month = fill2(dt.getMonth() + 1),
-  day = fill2(dt.getDate()),
-  hour = fill2(dt.getHours()),
-  mnts = fill2(dt.getMinutes()),
-  scnds = fill2(dt.getSeconds());
+let hour = fill2(dtNow.getHours()),
+  mnts = fill2(dtNow.getMinutes()),
+  scnds = fill2(dtNow.getSeconds());
 
-let strDate = `${hour}:${mnts}:${scnds} ${day}.${month}.${year}`;
+let strDate = `${hour}:${mnts}:${scnds} ${dateToString(dtNow)}`;
 console.log("Текущее время: " + strDate);
 let el = document.getElementById("time");
 el.textContent = "Текущее время: " + strDate;
@@ -21,10 +17,38 @@ function fill2(val) {
   return val;
 }
 
+// Функция, которая правильно выводит дату (без времени)
+function dateToString(dt) {
+  let year = "" + dt.getFullYear();
+  year = "0".repeat(4 - year.length) + year;
+
+  let month = fill2(dt.getMonth() + 1),
+    day = fill2(dt.getDate());
+
+  let ans = `${day}.${month}.${year}`;
+  return ans;
+}
+
+// Функция, которая корректирует неправильную строку даты
+function correctDateString(str) {
+  let parts = str.split(".");
+  for (let i = 0; i < parts.length; i++) {
+    if (parts[i].length < 2) {
+      parts[i] = "0" + parts[i];
+    }
+  }
+  let ans = parts.join(".");
+  return ans;
+}
+let incorrStr = "1.6.2018";
+console.log(`Корерктировка строки с датой. Было: "${incorrStr}". Стало: "${correctDateString("1.6.2018")}".`);
+
+
+
 
 // Напишите функцию, которая выводит на страницу текущий день недели на русском языке (реализацию выбираете сами)
 let dayNames = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-let today = dayNames[dt.getDay()];
+let today = dayNames[dtNow.getDay()];
 console.log("Текущий день недели: " + today);
 el = document.getElementById("today");
 el.textContent = "Текущий день недели: " + today;
@@ -34,10 +58,17 @@ el.textContent = "Текущий день недели: " + today;
 // Напишите функцию, которая выводит на страницу разницу между двумя датами в количестве дней
 // Prepare inputs
 let inputDt1 = document.getElementById("dt1"),
-  inputDt2 = document.getElementById("dt2");
+    inputDt2 = document.getElementById("dt2");
 
 inputDt1.addEventListener("change", onChangeDate);
 inputDt2.addEventListener("change", onChangeDate);
+
+// Get date components
+let year = "" + dtNow.getFullYear();
+year = "0".repeat(4 - year.length) + year;
+let month = fill2(dtNow.getMonth() + 1),
+  day = fill2(dtNow.getDate());
+
 strDate = `${year}-${month}-${day}T${hour}:${mnts}:${scnds}`; // Подготовить нужный формат данных
 inputDt1.value = strDate; // Установить значения
 inputDt2.value = strDate;
