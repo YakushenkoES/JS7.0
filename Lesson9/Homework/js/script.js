b3d855a74be5a661b6df92cd3c576d9cd48b0bda
Yakushenko Evgeniy <yakushenko.evgeny@gmail.com>
2019-05-06 09:08:48 +0300

    Lesson9 homework

--- Lesson9/Homework/js/script.js
+++ Lesson9/Homework/js/script.js
@@ -1,6 +1,29 @@
 window.addEventListener('DOMContentLoaded', function () {
 
   'use strict';
+  // Popup___________________________
+  let moreBtns = document.querySelectorAll(".more, .description-btn"),
+  overlay = document.querySelector(".overlay"),
+  close = document.querySelector(".popup-close");
+
+  let currDescBtn;
+  moreBtns.forEach(btn => {
+      btn.addEventListener("click", function(){
+      overlay.style.display = "block";
+      this.classList.add("more-splash");
+      document.body.style.overflow = "hidden";
+      currDescBtn = this;
+    });
+  });
+
+  close.addEventListener("click", function(){
+    overlay.style.display = "none";
+    document.body.style.overflow = "auto";
+    if(currDescBtn != undefined){
+      currDescBtn.classList.remove("more-splash");
+    }
+  });
+
   // Timer___________________________
   let deadLine = "2019-05-09";
 
