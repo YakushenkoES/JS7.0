ea6f5fc74d7dd6c872b0c8f322151f2914dc1046
Yakushenko Evgeniy <yakushenko.evgeny@gmail.com>
2019-05-07 22:05:14 +0300

    lesson11

--- Lesson11/Homework/js/script.js
+++ Lesson11/Homework/js/script.js
@@ -1,6 +1,67 @@
 window.addEventListener('DOMContentLoaded', () => {
 
   'use strict';
+
+  // Form ___________________________
+  let message = {
+    loading: 'Загрузка',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...'
+  };
+
+  let form = document.querySelector(".main-form"),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+
+  statusMessage.classList.add('status');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    form.appendChild(statusMessage);
+
+    let req = new XMLHttpRequest();
+
+    req.addEventListener('readystatechange', () => {
+      if (req.readyState < 4) {
+        statusMessage.textContent = message.loading;
+      } else if (req.readyState === 4 && req.status === 200) {
+        statusMessage.textContent = message.success;
+      } else {
+        statusMessage.textContent = message.failure;
+      }
+
+    });
+
+    // Заморачивался с openserver и кросс-доменными запросами, поэтому такой адрес
+    // могу забыть поменять на 'server.php'
+    req.open('POST', 'http://yoga.local/server.php');
+    // req.open('POST','server.php');
+
+
+    // Обычная форма
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    let formData = new FormData(form);
+    req.send(formData);
+
+    // JSON файлы
+    //req.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+    // Если ругается но ошибку кроссдоменных запросов (из-за 'application/json; charset=utf-8'), то можно
+    // req.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+    let obj={};
+    formData.forEach((value, key)=>{
+      obj[key] = value;
+    });
+    let strJSON = JSON.stringify(obj);
+    //req.send(strJSON);
+
+
+    for (let i = 0; i < input.length; i++) {
+      input[i].value = '';
+    }
+  });
+
+
+
   // Popup___________________________
   let moreBtns = document.querySelectorAll(".more, .description-btn"),
     overlay = document.querySelector(".overlay"),
