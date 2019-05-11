700b18a99153a161caee5d7680a23c99bfd1ab9d
Yakushenko Evgeniy <yakushenko.evgeny@gmail.com>
2019-05-11 20:48:59 +0300

    fix

--- Bonus Lesson 3 JS Test Mocha Chai/Test/script.mocha_specification.js
+++ Bonus Lesson 3 JS Test Mocha Chai/Test/script.mocha_specification.js
@@ -1,5 +1,5 @@
-describe("Функция sum",function(){
-	it('Сумма чисел 2 и 2 НЕ больше 10.', function () {
+describe("Функция sum", function () {
+  it('Сумма чисел 2 и 2 НЕ больше 10.', function () {
     assert.isFalse(sum(2, 2));
   });
   it('Сумма чисел 5 и 5 НЕ больше 10.', function () {
@@ -10,28 +10,28 @@ describe("Функция sum",function(){
   });
 });
 
-describe("Переменная num",function(){
-	it('Переменная num должна быть равна 5', function () {
+describe("Переменная num", function () {
+  it('Переменная num должна быть равна 5', function () {
     assert.equal(num, 5);
   });
 });
 
-describe("Функция each",function(){
+describe("Функция each", function () {
 
   let input = [64, 49, 36, 25, 16];
-  let expect = [8,7,6,5,4];
+  let expect = [8, 7, 6, 5, 4];
 
   let res = each(input, myFunc);
 
-	it('Проверка на возвращаемый тип "массив"', function () {
+  it('Проверка на возвращаемый тип "массив"', function () {
     assert.isArray(res);
   });
 
   it('Проверка длины массива', function () {
-    assert.lengthOf(res,expect.length);
+    assert.lengthOf(res, expect.length);
   });
 
   it('Проверка на равенство массивов', function () {
-    assert.deepEqual(res,expect);
+    assert.deepEqual(res, expect);
   });
 });
